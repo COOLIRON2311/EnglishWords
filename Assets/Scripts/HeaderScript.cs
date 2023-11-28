ad8b6feebf2c413b0c1cb17b777777e5a50429f9
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2023-11-28 22:36:33 +0300

    S1Learn layout

--- Assets/Scripts/HeaderScript.cs
+++ Assets/Scripts/HeaderScript.cs
@@ -7,16 +7,6 @@ using UnityEngine.UI;
 
 public class HeaderScript : MonoBehaviour
 {
-    enum Scenes : int
-    {
-        Exit = -1,
-        S0Main = 0,
-        S1Learn = 1,
-        S2Control = 2,
-        S3Results = 3,
-        S4Settings = 4,
-        S5Help = 5
-    }
     Button lBut, rBut;
     /// <summary>
     /// current scene index
---
+++ Assets/Scripts/LearnScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LearnScript : MonoBehaviour
+{
+    public DataScript data;
+    public Button mainButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Setup DropDown
+        var dd = GameObject.Find("MainDropdown").GetComponent<Dropdown>();
+        dd.value = data.Level;
+        dd.onValueChanged.AddListener(OnValueChangeHandler);
+
+        // Create buttons with current level topics
+        for (int i = 0; i < data.TopicCount; i++)
+        {
+            var b = Instantiate(mainButton);
+            b.GetComponentInChildren<Text>().text = data.Topic(i);
+            b.transform.SetParent(transform);
+            b.transform.localScale = Vector2.one;
+        }
+
+        // Set selection to first button
+        var es = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+        es.SetSelectedGameObject(transform.GetChild(0).gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.inputString.Length > 0)
+        {
+            int i = Input.inputString[0] - '1';
+            if (i >= 0 && i <= 3)
+                OnValueChangeHandler(i);
+        }
+    }
+
+    void OnValueChangeHandler(int i)
+    {
+        if (i != data.Level)
+        {
+            data.SetLevel(i);
+            // Reload scene to refresh its contents
+            SceneManager.LoadScene((int)Scenes.S1Learn);
+        }
+    }
+}
